7656eb3ec738e0508ff7c56b5cf241535aae5c4b
Dongwoo Yang <ckck803@gmail.com>
2021-02-02 11:05:15 +0900

    feat : OAuth인증후 User데이터 저장 기능 구현
    - Google로그인을 진행한 후 UserInfo Endpoint에 접근해 사용자 정보를 가져온다.
    - 가져온 정보를 토데로 DB에 접근해 사용자 정보를 저장 or 갱신한다.

--- security-oauth2/src/main/java/com/example/sampleoauth2/security/SecurityConfig.java
+++ security-oauth2/src/main/java/com/example/sampleoauth2/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.sampleoauth2.security;
 
+import com.example.sampleoauth2.security.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,6 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         http
-                .oauth2Login();
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
     }
 }
